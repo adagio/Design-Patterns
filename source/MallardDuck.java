8e04106962a4ac4d3f1ef840e0a74b7b11876fdf
Franco Cedillo <franco.cedillo@gmail.com>
2012-11-19 06:45:35 -0500

    source code added

---
+++ source/MallardDuck.java
@@ -0,0 +1,12 @@
+public class MallardDuck extends Duck {
+
+	public MallardDuck(){
+		quackBehavior = new Quack();
+		flyBehavior = new FlyWithWings();
+	}
+
+	public void display() {
+		System.out.println("I´m a real Mallard duck");
+	}
+
+}
