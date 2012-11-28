f81d0225c87691aaa453cf3822307bb2b777b457
Franco Cedillo <franco.cedillo@gmail.com>
2012-11-28 17:14:42 -0500

    ModelDuck (and rocket-enabled) added to Simulator

--- source/MiniDuckSimulator.java
+++ source/MiniDuckSimulator.java
@@ -1,7 +1,17 @@
 public class MiniDuckSimulator {
+	
+	
 	public static void main(String[] args) {
+		
 		Duck mallard = new MallardDuck();
 		mallard.performQuack();
 		mallard.performFly();
+		
+		Duck model = new ModelDuck();
+		model.performFly();
+		model.setFlyBehavior(new FlyRocketPowered());
+		model.performFly();
+		
 	}
+	
 }
